0e8410f43107a9e637aa34c2d34fa9f97443f921
HamzaAli2202 <ui.hamzaali22@gmail.com>
2023-11-12 10:46:05 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Card, CardContent } from "@mui/material";
+function App() {
+  return (
+    <Card>
+      <CardContent></CardContent>
+    </Card>
+  );
+}
+
+export default App;
